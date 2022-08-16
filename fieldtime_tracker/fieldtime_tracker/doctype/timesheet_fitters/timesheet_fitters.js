dd8cc6fcb8b73361c6da1bc3ac4db2eaa45d961b
estackhub <spryng.managed@gmail.com>
2022-08-16 06:08:35 +0000

    Second client script to calculate total work hourfrom all doctype field

--- fieldtime_tracker/fieldtime_tracker/doctype/timesheet_fitters/timesheet_fitters.js
+++ fieldtime_tracker/fieldtime_tracker/doctype/timesheet_fitters/timesheet_fitters.js
@@ -5,31 +5,75 @@ frappe.ui.form.on('Timesheet Fitters', {
 	// refresh: function(frm) {
 
 	// }
+	setup: function(frm){
+
+		frm.get_projects_hours = function(frm){
+			let total_hour = 0;			
+			frm.doc.time_logs.forEach(tm=>{
+				total_hour += flt(tm.hours_spend);				
+				
+			})
+			frm.set_value("total_project_time",total_hour);
+			// total_work_time			
+			
+		}
+
+		frm.get_total_hours = function(frm){
+			let total_hour = 0;
+			total_hour = frm.doc.total_project_time - frm.doc.break_time - (1/2*(frm.doc.travel_time-frm.doc.driver_time))
+			frm.set_value("total_work_time",total_hour);
+			
+			
+		}
+	},
+	break_time:function(frm){
+		//stat
+		frm.get_total_hours(frm);
+		
+		frm.refresh_field();
+	},
+	travel_time:function(frm){
+		//stat
+		frm.get_total_hours(frm);
+		
+		frm.refresh_field();
+	},
+	driver_time:function(frm){
+		//stat
+		frm.get_total_hours(frm);
+		
+		frm.refresh_field();
+	},
+
 });
 
 frappe.ui.form.on("Hours Fitters Project", {	
 
 	from_time: function(frm, cdt, cdn) {
-		//calculate_ento_timed_time(frm, cdt, cdn);
+		calculate_ento_timed_time(frm, cdt, cdn);
 	},
 
 	to_time: function(frm, cdt, cdn) {
 		
 		var child = locals[cdt][cdn];
 
-		//if(frm._setting_hours) return;
-		
+		if(frm._setting_hours) return;		
 
 		var hours = moment(child.to_time).diff(moment(child.from_time), "seconds") / 3600;
-		//var hours = moment("04:02:24").diff(moment("02:11:29"), "hours");
-		console.log('got: ',hours);
 		
 		frappe.model.set_value(cdt, cdn, "hours_spend", hours);
-		//frappe.model.set_value(cdt, cdn, "hours_spend", 3.0);
+
+
+		frm.get_projects_hours(frm);
+		
+		frm.refresh_field('time_logs');
 	},
 
 	hours_spend: function(frm, cdt, cdn) {
 		calculate_end_time(frm, cdt, cdn);
+
+		/* frm.get_total_hours(frm);		
+		frm.refresh_field('time_logs'); */
 	},
 });
 
